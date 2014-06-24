15988c34e624cb23cb36ed79e878855783509e6e
Mastajeet <cyberyder@hotmail.com>
2014-06-24 19:57:39 -0400

    24 juin 2014 - Ajout de l'historique des horaires pour les employés

--- employe_horshift.php
+++ employe_horshift.php
@@ -17,10 +17,20 @@ $MainOutput->OpenTable();
 		$MainOutput->CloseCol();
 		$MainOutput->CloseRow();
 
-		
-		$MainOutput->OpenRow();
+
+
+$MainOutput->OpenRow();
+$MainOutput->OpenCol();
+$MainOutput->addpic('carlos.gif','height=30');
+$MainOutput->CloseCol();
+$MainOutput->CloseRow();
+
+
+
+
+$MainOutput->OpenRow();
 		$MainOutput->OpenCol();
-			$MainOutput->AddTexte('Mon avec Remplacement','Titre');
+			$MainOutput->AddTexte('Mon horaire futur avec Remplacement','Titre');
 		$MainOutput->CloseCol();
 		$MainOutput->CloseRow();
 		
@@ -31,7 +41,11 @@ $SemainePreformat[0] = get_last_sunday();
 $SemainePreformat[1]= get_next_sunday(0, $SemainePreformat[0]);
 $SemainePreformat[2]= get_next_sunday(0, $SemainePreformat[1]);
 $SemainePreformat[3]= get_next_sunday(0, $SemainePreformat[2]);
-        
+
+
+
+
+
 $Semaine = array($SemainePreformat[0],$SemainePreformat[1],$SemainePreformat[2],$SemainePreformat[3]);
         
         
@@ -40,9 +54,48 @@ $Semaine = array($SemainePreformat[0],$SemainePreformat[1],$SemainePreformat[2],
 		$MainOutput->OpenRow();
 		$MainOutput->OpenCol();
 		$MainOutput->AddOutput(get_employe_horaire($_GET['IDEmploye'],$Semaine),0,0);
-	
 		$MainOutput->CloseCol();
 		$MainOutput->CloseRow();
+
+#Ajout 24 juin 2014 - 1 mois d'historique sur les horaire
+
+
+$MainOutput->OpenRow();
+$MainOutput->OpenCol();
+$MainOutput->addpic('carlos.gif','height=30');
+$MainOutput->CloseCol();
+$MainOutput->CloseRow();
+
+
+$MainOutput->OpenRow();
+$MainOutput->OpenCol();
+$MainOutput->AddTexte('Mon horaire passé','Titre');
+$MainOutput->CloseCol();
+$MainOutput->CloseRow();
+
+
+
+$SemainePreformat = array();
+$SemainePreformat[2] = get_last_sunday(1);
+$SemainePreformat[1]= get_last_sunday(0, $SemainePreformat[2]-3600);
+$SemainePreformat[0]= get_last_sunday(0, $SemainePreformat[1]-3600);
+
+
+
+
+
+
+$Semaine = array($SemainePreformat[2],$SemainePreformat[1],$SemainePreformat[0]);
+
+
+
+$MainOutput->OpenRow();
+$MainOutput->OpenCol();
+$MainOutput->AddOutput(get_employe_horaire($_GET['IDEmploye'],$Semaine),0,0);
+$MainOutput->CloseCol();
+$MainOutput->CloseRow();
+
+
 
 
 
