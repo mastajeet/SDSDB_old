305da81dd169c1347693566e6b22eaddb41d4e0a
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-01-17 16:53:15 -0500

    correction pour le linkage des shift sur la facture

--- generate_facture.php
+++ generate_facture.php
@@ -23,20 +23,20 @@ if(isset($_GET['Semaine']) && !isset($_POST['FORMGenerateCote'])){
 		$MainOutput->inputhidden_env('Section','Generate_Facture');
 		$MainOutput->inputhidden_env('Semaine',$_GET['Semaine']);
 		$MainOutput->InputRadio('GenerateCote',$Installation,'','Piscine','VER');
-		$MainOutput->formsubmit('Créer');
+		$MainOutput->formsubmit('Crï¿½er');
 	$MainOutput->CloseCol();
 	$MainOutput->CloseRow();
 	$MainOutput->CloseTable();
 }elseif(isset($_POST['Semaine']) AND isset($_POST['FORMGenerateCote'])){
 $_POST['FORMCote'] = $_POST['FORMGenerateCote'];
-//IL FAUT CHéKER SI LES HEURES ONT PAS DEJA éTé FACTUéES
+//IL FAUT CHï¿½KER SI LES HEURES ONT PAS DEJA ï¿½Tï¿½ FACTUï¿½ES
 $Req = "SELECT sum(Facture) as a FROM shift LEFT JOIN installation on shift.IDInstallation = installation.IDInstallation WHERE `Semaine`=".$_POST['Semaine']." AND installation.Cote='".$_POST['FORMCote']."' GROUP BY Cote";
 $SQL->select($Req);
 $Rep = $SQL->FetchArray();
 if($Rep[0]>0){
-	$MainOutput->AddTexte('La facture pour cette période est déjé faite','Warning');
+	$MainOutput->AddTexte('La facture pour cette pï¿½riode est dï¿½jï¿½ faite','Warning');
 }elseif($_POST['Semaine']>=get_last_sunday(0,time())){
-	$MainOutput->AddTexte('Vous ne pouvez pas faire la facturation pour une période non complétée','Warning');
+	$MainOutput->AddTexte('Vous ne pouvez pas faire la facturation pour une pï¿½riode non complï¿½tï¿½e','Warning');
 }else{
 	$SQLins = new sqlclass();
 	$ReqIDIns = "SELECT DISTINCT shift.IDInstallation FROM shift JOIN installation on shift.IDInstallation = installation.IDInstallation WHERE installation.Cote='".$_POST['FORMCote']."' AND Semaine=".$_POST['Semaine']." ORDER BY Nom ASC";
@@ -50,8 +50,8 @@ if($Rep[0]>0){
 		while($Rep = $SQL->FetchArray()){
 				$Titre = "Sauveteur";
 			if($Rep[5])
-				$Titre = "Deuxième Sauveteur";
-			if($i>0 && $Shift[$i-1]['End'] == $Rep[0]){
+				$Titre = "Deuxiï¿½me Sauveteur";
+			if($i>0 && ($Shift[$i-1]['End'] == $Rep[0]) and ($Shift[$i-1]['Jour']==$Rep[2])){
 				$Shift[$i-1]['End'] = $Rep[1];
                 $Shift[$i-1]['Notes'] = substr($Shift[$i-1]['Notes'],0,-1);
                 $Shift[$i-1]['Notes'] .= "-".get_employe_initials($Rep[7]).")";
@@ -65,20 +65,17 @@ if($Rep[0]>0){
 		$IDFacture = add_facture($_POST['FORMCote'],$_POST['Semaine']);
 
 		foreach($Shift as $v){
-			
-			
+
 			$v['End'] = $v['End'] - bcmod($v['End'],36);
 			$v['Start'] = $v['Start'] - bcmod($v['Start'],36);
-			
-			
-			
+
 			if($v['Start']==0 and $v['End']==14400)
 				$v['Notes'] = $v['Notes']." (Minimum 4h)";
 				
 			if(is_ferie($v['Jour']*86400+$_POST['Semaine'])){
 				if($v['Ferie']<>1){
 					$v['TXH'] = $v['TXH']*$v['Ferie'];
-					$v['Notes'] = $v['Notes']." (x".$v['Ferie']." Journée Fériée)";
+					$v['Notes'] = $v['Notes']." (x".$v['Ferie']." Journï¿½e Fï¿½riï¿½e)";
 				}
 			}
 		$Req = "INSERT INTO factsheet(`IDFacture`,`Start`,`End`,`Jour`,`TXH`,`Notes`) VALUES(".$IDFacture.",'".$v['Start']."','".$v['End']."','".$v['Jour']."','".$v['TXH']."','".addslashes($v['Notes'])."')";
