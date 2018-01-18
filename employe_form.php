6b86d8ed27e04aba4bfcaa81af2dac0f6757ff4b
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-01-18 14:16:54 -0500

    GESTION UTF-8 LATIN1

--- employe_form.php
+++ employe_form.php
@@ -1,12 +1,28 @@
 <?PHP
-$MainOutput->addform('Ajouter / Modifier un employï¿½');
+const EMPLOYEE_NUMBER = 'Numéro d\'employé';
+const ADD_MODIFY_EMPLOYEE = 'Ajouter / Modifier un employé';
+const NAME = 'Nom';
+const SURNAME = 'Prénom';
+const SCHEDULE_NAME = 'Nom Horaire';
+const DATE_OF_BIRTH = 'Date de naissance';
+const SOCIAL_SECURITY_NUMBER = 'Numéro d\assurance sociale';
+const NOTES = 'Notes';
+const PHONE_PRINCIPAL = 'Tél. Principal';
+const PHONE_SECONDARY = 'Tél. Autre';
+const CELLPHONE = 'Cellulaire';
+const PAGET = 'Paget';
+const LEAVING_REASON = 'Raison du départ';
+const ADD_MODIFY = 'Ajouter / Modifier';
+
+
+$MainOutput->addform(ADD_MODIFY_EMPLOYEE);
 $MainOutput->inputhidden_env('Action','Employe');
 if(isset($_GET['IDEmploye'])){
 	$Info = get_employe_info($_GET['IDEmploye']);
 	$MainOutput->inputhidden_env('IDEmploye',$_GET['IDEmploye']);
 	$MainOutput->inputhidden_env('Update',TRUE);
 }else{
-	$Info = array('IDEmploye'=>'','HName'=>'','Ville'=>'Quï¿½0bec','Status'=>'','NAS'=>'','Nom'=>'','Prenom'=>'','Session'=>get_vars('Saison'),'DateNaissance'=>0,'Adresse'=>'','CodePostal'=>'','Email'=>'','TelM'=>'','TelP'=>'','TelA'=>'','Cell'=>'','Paget'=>'','IDSecteur'=>'','Cessation'=>'','Notes'=>'','Raison'=>'','SalaireB'=>'9.50','SalaireS'=>'9.75','SalaireA'=>'9.25','DateEmbauche'=>0,'Engage'=>1,'EAssistant'=>'');
+	$Info = array('IDEmploye'=>'','HName'=>'','Ville'=>'Québec','Status'=>'','NAS'=>'', NAME =>'','Prenom'=>'','Session'=>get_vars('Saison'),'DateNaissance'=>0,'Adresse'=>'','CodePostal'=>'','Email'=>'','TelM'=>'','TelP'=>'','TelA'=>'','Cell'=>'', PAGET =>'','IDSecteur'=>'','Cessation'=>'', NOTES =>'','Raison'=>'','SalaireB'=>'9.50','SalaireS'=>'9.75','SalaireA'=>'9.25','DateEmbauche'=>0,'Engage'=>1,'EAssistant'=>'');
 	$MainOutput->inputhidden_env('Update',FALSE);
 }
 
@@ -17,7 +33,7 @@ $MainOutput->addlink('index.php?Section=Employe_Report&IDEmploye='.$Info['IDEmpl
 $MainOutput->addlink('index.php?Section=Employe_Horshift&IDEmploye='.$Info['IDEmploye'],'<img src=b_fact.png border=0>');
 $MainOutput->addlink('index.php?Section=Display_AskedRemplacement&IDEmploye='.$Info['IDEmploye'],'<img src=b_del.png border=0>');
 
-$MainOutput->inputtext('IDEmploye','Numï¿½ro d\'employï¿½','3',$Info['IDEmploye']);
+$MainOutput->inputtext('IDEmploye', EMPLOYEE_NUMBER,'3',$Info['IDEmploye']);
 
 
 $MainOutput->OpenRow();
@@ -26,16 +42,16 @@ $MainOutput->OpenCol('100%',2);
 $MainOutput->CloseCol();
 $MainOutput->CloseRow();
 
-$MainOutput->inputtext('Nom','Nom','28',$Info['Nom']);
-$MainOutput->inputtext('Prenom','Prï¿½nom','28',$Info['Prenom']);
-$MainOutput->inputtext('HName','Nom Horaire','28',$Info['HName']);
-$MainOutput->inputtime('DateNaissance','Date de naissance',$Info['DateNaissance'],array('Date'=>TRUE,'Time'=>FALSE));
-$MainOutput->inputtext('NAS','Numï¿½ro d\assurance sociale','9',$Info['NAS']);
-$MainOutput->textarea('Notes','Notes','25','2',$Info['Notes']);
+$MainOutput->inputtext(NAME, NAME,'28',$Info[NAME]);
+$MainOutput->inputtext('Prenom', SURNAME,'28',$Info['Prenom']);
+$MainOutput->inputtext('HName', SCHEDULE_NAME,'28',$Info['HName']);
+$MainOutput->inputtime('DateNaissance', DATE_OF_BIRTH,$Info['DateNaissance'],array('Date'=>TRUE,'Time'=>FALSE));
+$MainOutput->inputtext('NAS', SOCIAL_SECURITY_NUMBER,'9',$Info['NAS']);
+$MainOutput->textarea(NOTES, NOTES,'25','2',$Info[NOTES]);
 
 
 
-//check s'il y a des vacances ï¿½ venir
+//check s'il y a des vacances ? venir
 $vacances_threshold = time()-3600*24;
 $Req = "SELECT * FROM vacances WHERE FinVacances >= ".$vacances_threshold." and IDEmploye = ".$Info['IDEmploye']." ORDER BY DebutVacances ASC";
 $SQL = new sqlclass();
@@ -77,10 +93,10 @@ $MainOutput->inputselect('IDSecteur',$Req,$Info['IDSecteur'],'Secteur');
 $MainOutput->inputtext('Ville','Ville','28',$Info['Ville']);
 $MainOutput->inputtext('CodePostal','Code Postal','7',$Info['CodePostal']);
 $MainOutput->inputtext('Email','Courriel','28',$Info['Email']);
-$MainOutput->inputphone('TelP','Tï¿½l. Principal',$Info['TelP']);
-$MainOutput->inputphone('TelA','Tï¿½l. Autre',$Info['TelA']);
-$MainOutput->inputphone('Cell','Cellulaire',$Info['Cell']);
-$MainOutput->inputphone('Paget','Paget',$Info['Paget']);
+$MainOutput->inputphone('TelP', PHONE_PRINCIPAL,$Info['TelP']);
+$MainOutput->inputphone('TelA', PHONE_SECONDARY,$Info['TelA']);
+$MainOutput->inputphone('Cell', CELLPHONE,$Info['Cell']);
+$MainOutput->inputphone(PAGET, PAGET,$Info['Paget']);
 
 
 $MainOutput->OpenRow();
@@ -90,7 +106,7 @@ $MainOutput->CloseCol();
 $MainOutput->CloseRow();
 
 $MainOutput->inputtime('DateEmbauche','Date d\'embauche',$Info['DateEmbauche'],array('Date'=>TRUE,'Time'=>FALSE));
-$Status = array('Temps plein'=>'Temps plein','Secondaire'=>'Secondaire','Cï¿½GEP'=>'Cï¿½GEP','Universitï¿½'=>'Universitï¿½','Bureau'=>'Bureau');
+$Status = array('Temps plein'=>'Temps plein','Secondaire'=>'Secondaire','C?GEP'=>'C?GEP','Universit?'=>'Universit?','Bureau'=>'Bureau');
 $Session = get_saison_list();
 $Saison = array();
 foreach($Session as $v){
@@ -103,24 +119,10 @@ $MainOutput->inputtext('SalaireB','Salaire Bureau','5',$Info['SalaireB']);
 $MainOutput->inputtext('SalaireS','Salaire Sauveteur','5',$Info['SalaireS']);
 $MainOutput->inputtext('SalaireA','Salaire Assitant','5',$Info['SalaireA']);
 $MainOutput->flag('Cessation',$Info['Cessation']);
-$MainOutput->textarea('Raison','Raison du dï¿½part','25','2',$Info['Raison']);
+$MainOutput->textarea('Raison', LEAVING_REASON,'25','2',$Info['Raison']);
 
 
-
-/*
-$Path = strstr($_SERVER['HTTP_REFERER'],'?');
-$Path = substr($Path,1);
-$Path = explode('&',$Path);
-
-foreach($Path as $v){
-	if($Var = explode('=',$v))
-		$MainOutput->inputhidden_env($Var[0],$Var[1]);
-}
-
-*/
-
-
-$MainOutput->formsubmit('Ajouter / Modifier');
+$MainOutput->formsubmit(ADD_MODIFY);
 echo $MainOutput->send(1);
 
 ?>
