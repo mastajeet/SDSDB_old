6ec79fbcbda41383a48fdd74f5ea2b9062c0f58a
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-02-03 11:46:39 -0500

    refactor pour le style

--- generate_facture.php
+++ generate_facture.php
@@ -8,9 +8,7 @@ if(isset($_GET['Semaine']) && !isset($_POST['FORMGenerateCote'])){
 	$MainOutput->AddTexte('Semaine du '.$ENDS['Start'].' au '.$ENDS['End'],'Titre');
  	$MainOutput->CloseCol();
 	$MainOutput->CloseRow();
-
     $installation_to_bill = Installation::get_installations_in_string_to_bill($_GET['Semaine']);
-
 	$MainOutput->OpenRow();
 	$MainOutput->Opencol();
 		$MainOutput->addform(AJOUTER_UNE_FACTURE);
@@ -23,22 +21,16 @@ if(isset($_GET['Semaine']) && !isset($_POST['FORMGenerateCote'])){
 	$MainOutput->CloseTable();
 }elseif(isset($_POST['Semaine']) AND isset($_POST['FORMGenerateCote'])){
     $_POST['FORMCote'] = $_POST['FORMGenerateCote'];
-
     $installation_to_bill = Installation::get_installations_to_bill($_POST['FORMCote'], $_POST['Semaine']);
     if(count($installation_to_bill)==0){
         $MainOutput->AddTexte(ALREADY_BILLED,'Warning');
     }elseif($_POST['Semaine'] >= get_last_sunday(0,time()) ) {
         $MainOutput->AddTexte(INCOMPLETE_PERIOD, 'Warning');
     }else{
-
-
         $facture = Facture::generate_facture($_POST['FORMCote'],$_POST['Semaine']);
-
-
         $Modifie=TRUE;
         $_GET['IDFacture'] = $facture->IDFacture;
         include_once('display_facture.php');
 	}
-	
 }
 echo $MainOutput->send(1);
