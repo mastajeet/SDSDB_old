bbf96c0f0ba5b1785ed7bf3dd112f2597f6e0847
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-06-06 16:43:31 -0400

    previous commit part2

---
+++ view/installation/display_installation_information.php
@@ -0,0 +1,75 @@
+<?php
+
+    //$installation variable is passed by controller
+
+
+    $Info = get_installation_info($installation->IDInstallation);
+    $SQL = new sqlclass();
+    
+    $MainOutput->opentable('500');
+    $MainOutput->openrow();
+    $MainOutput->opencol('100%',2);
+    $MainOutput->AddLink($installation->Lien,$installation->Nom,"_BLANK",'Titre');
+    $MainOutput->AddTexte("&nbsp; (".$installation->Cote.")",'Titre');
+    $MainOutput->AddLink('index.php?Section=Installation_Form&IDInstallation='.$installation->IDInstallation,'<img src=b_edit.png border=0>');
+    $MainOutput->AddLink('index.php?Section=Horshift&IDInstallation='.$installation->IDInstallation,'<img src=b_save.png border=0>');
+
+    if($authorization->verifySuperAdmin($_COOKIE)){
+        $MainOutput->AddLink('index.php?Cote='.$installation->Cote,'<img src=b_fact.png border=0>');
+    }
+    $MainOutput->AddLink('index.php?Section=ClientComment_Form&IDInstallation='.$installation->IDInstallation,'<img src=b_conf.png border=0>');
+    $MainOutput->closecol();
+    $MainOutput->closerow();
+
+
+    $MainOutput->openrow();
+    $MainOutput->opencol();
+    $MainOutput->AddTexte("Type: ".array_get(ConstantArray::get_installation_type_kvp()[$installation->IDType])."
+		Punch: ".get_flag($installation->Punch)."
+		Assistant: ".get_flag($installation->Assistant)."
+		Cadenas SdS: ".get_flag($installation->Cadenas),'Texte');
+    $MainOutput->closecol();
+    $MainOutput->opencol();
+    $MainOutput->AddTexte("Dernière Inspection: à  venir"."
+    Stationnement: ". array_get(ConstantArray::get_installation_parking_type_kvp()[$installation->Stationnement]));
+    $MainOutput->br();
+
+    $MainOutput->closecol();
+    $MainOutput->closerow();
+
+
+
+    $MainOutput->Openrow();
+    $MainOutput->OpenCol('50%');
+    $MainOutput->addoutput(format_responsable($installation->IDResponsable,'de la piscine',$installation->IDClient),0,0);
+    $MainOutput->closecol();
+    $MainOutput->OpenCol('50%');
+    $MainOutput->AddTexte('Adresse de la piscine','Titre');
+    $MainOutput->br();
+    $MainOutput->AddTexte($installation->Adresse);
+    if(strlen($installation->Tel)>4){
+        $MainOutput->br();
+        $MainOutput->AddTexte("Tel.: (".substr($installation->Tel,0,3).") ".substr($installation->Tel,3,3)."-".substr($installation->Tel,6,4));
+        if(strlen(substr($installation->Tel,10,4))>1)
+            $MainOutput->AddTexte(" #".substr($installation->Tel,10,4));
+    }
+    $MainOutput->closecol();
+    $MainOutput->closerow();
+    if($installation->Notes<>""){
+        $MainOutput->openrow();
+        $MainOutput->opencol('100%',2);
+        $MainOutput->AddTexte("Notes: ".$installation->Notes,'Texte');
+        $MainOutput->closecol();
+        $MainOutput->closerow();
+    }
+    if($installation->Toilettes<>""){
+        $MainOutput->openrow();
+        $MainOutput->opencol('100%',2);
+        $MainOutput->AddTexte("Toilettes: ".$installation->Toilettes,'Texte');
+        $MainOutput->closecol();
+        $MainOutput->closerow();
+    }
+
+
+    $MainOutput->CloseTable();
+
