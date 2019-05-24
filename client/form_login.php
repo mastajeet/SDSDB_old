e145b37e5f879b73ae1de2ca8294965a76ff525b
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-05-24 12:30:15 -0400

    ajouter 3riv - vraiment pas clean comme modif

--- client/form_login.php
+++ client/form_login.php
@@ -3,7 +3,7 @@ $MainOutput->br(2);
 $MainOutput->AddForm('Veuillez entrer vos identifiants');
 $MainOutput->inputHidden_env('Action','Login');
 
-$MainOutput->InputSelect('CIE',array('QC'=>'Québec','MTL'=>'Montréal'),'QC','Région');
+$MainOutput->InputSelect('CIE',array('QC'=>'Québec','MTL'=>'Montréal','TR'=>'Trois-Rivières'),'QC','Région');
 $MainOutput->inputText('Cote','Cote&nbsp;de&nbsp;Facturation',5);
 $MainOutput->InputPassword('Password','Mot de passe');
 $MainOutput->FormSubmit('Connexion');
