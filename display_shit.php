25a92e0edbb3cd3a1c31fe9f7e6338b28270761b
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2019-09-04 20:32:04 -0400

    correction pou

--- display_shit.php
+++ display_shit.php
@@ -43,7 +43,7 @@ if(!$_GET['ToPrint']){
 	$MainOutput->CloseCol();
 	$MainOutput->OpenCol('450',3);
 	$MainOutput->addoutput('<div align=left>',0,0);
-	$MainOutput->AddLink('index.php?Section=Display_Shit&Semaine='.get_last_sunday(1,$_GET['Semaine']),'Semaine prÃ©cÃ©dente');
+	$MainOutput->AddLink('index.php?Section=Display_Shit&Semaine='.get_last_sunday(1,$_GET['Semaine']),'Semaine Précédente');
 	$MainOutput->addoutput('</div>',0,0);
 	$MainOutput->CloseCol();
 	$MainOutput->OpenCol('450',3);
@@ -128,7 +128,7 @@ while($Rep = $SQL->FetchArray()){
 			$End['i']="";
 		$Output[$Rep2['Jour']]->openrow();
 		$Output[$Rep2['Jour']]->opencol();
-		$Output[$Rep2['Jour']]->addtexte($Start['G']."h".$Start['i']."&nbsp;Ã &nbsp;".$End['G']."h".$End['i'], 'Titre2');
+		$Output[$Rep2['Jour']]->addtexte($Start['G']."h".$Start['i']."&nbsp;à&nbsp;".$End['G']."h".$End['i'], 'Titre2');
 		if(!$_GET['ToPrint'])
 			$Output[$Rep2['Jour']]->AddLink('index.php?Section=Shift_Form&IDShift='.$Rep2['IDShift'],'<img src=b_edit.png border=0>');
 		$Output[$Rep2['Jour']]->closecol();
